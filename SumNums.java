025e725bef5450a1e454a46a3d6f1ccf77845503
SadineniAbhi <abhisadineni@gmail.com>
2023-10-03 09:32:03 +0530

    sum of two nums

---
+++ SumNums.java
@@ -0,0 +1,13 @@
+class SumNums{
+    public void sum(){
+        int a,b,c;
+        a = 10;
+        b = 20; 
+        c = a+b;
+        System.out.println(c);
+    }
+    public static void main(String[] args) {
+        SumNums s = new SumNums();
+        s.sum();
+    }
+}
