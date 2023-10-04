108408ea17d6febfa7bd1bd4bb26e48a1580f36c
SadineniAbhi <abhisadineni@gmail.com>
2023-10-04 23:16:53 +0530

    test

--- HareKrishna.java
+++ HareKrishna.java
@@ -1,5 +1,5 @@
 public class HareKrishna {
     public static void main(String[] args){
-        System.out.println("Hare krishna");
+        System.out.println("Hare krishna form Ubuntu!!");
     }
 }
